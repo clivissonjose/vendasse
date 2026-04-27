e588df4ab89599573cb20ac4ffdc00336fd98c60
Clivisson <cjbarbosa2001@gmail.com>
2026-04-27 12:25:30 -0300

    feat: UserRepository and AuthenticationService

---
+++ src/main/java/com/vendasse/services/AuthorizationService.java
@@ -0,0 +1,23 @@
+package com.vendasse.services;
+
+import com.vendasse.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public AuthorizationService (UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
+
+}
